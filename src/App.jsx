06be2efa90409723ed196d28ae4d7fde906eff75
cmccollum325 <cmccollum325@gmail.com>
2023-07-24 13:31:52 -0500

    Render Puppy names via react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
-// function () {
+function App () {
   
+ 
 
-//   return (
-//     <>
-//       <div>
+  const [puppies, setPuppies] = useState(puppyList);
 
-//       </div>
-//     </>
-//   )
-// }
+  console.log('puppyList', puppyList);
+  
+   return (
+    <>
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+        }
+      </div>
+    </>
+  )
+}
 
 export default App
+
